1411d9f33f93cbc91b0798790a321ec562a104b1
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 16:50:35 +0530

    created discriminator model

--- dcgan.py
+++ dcgan.py
@@ -18,3 +18,21 @@ def lrelu(x):
 def binary_cross_entropy(x, z):
     eps = 1e-12
     return (-(x * tf.log(z + eps) + (1. - x) * tf.log(1. - z + eps)))
+
+
+#dicriminator model
+
+def discriminator(img_in, reuse=None, keep_prob=keep_prob):
+    activation = lrelu
+    with tf.variable_scope("discriminator", reuse=reuse):
+        x = tf.reshape(img_in, shape=[-1, 28, 28, 1])
+        x = tf.layers.conv2d(x, kernel_size=5, filters=64, strides=2, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.layers.conv2d(x, kernel_size=5, filters=64, strides=1, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.layers.conv2d(x, kernel_size=5, filters=64, strides=1, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.flatten(x)
+        x = tf.layers.dense(x, units=128, activation=activation)
+        x = tf.layers.dense(x, units=1, activation=tf.nn.sigmoid)
+        return x
