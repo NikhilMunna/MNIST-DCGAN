d876e0b54829c69a4d6db3d16f1b07e4064fb6e4
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 16:47:30 +0530

    place holders

--- dcgan.py
+++ dcgan.py
@@ -3,3 +3,9 @@ mnist = input_data.read_data_sets('MNIST_data')
 tf.reset_default_graph()
 batch_size = 64
 n_noise = 64
+
+X_in = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='X')
+noise = tf.placeholder(dtype=tf.float32, shape=[None, n_noise])
+
+keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob')
+is_training = tf.placeholder(dtype=tf.bool, name='is_training')
