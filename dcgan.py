11045cc3705063093a5b84fc856ba1a79a985382
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 16:54:32 +0530

    created generator model

--- dcgan.py
+++ dcgan.py
@@ -36,3 +36,28 @@ def discriminator(img_in, reuse=None, keep_prob=keep_prob):
         x = tf.layers.dense(x, units=128, activation=activation)
         x = tf.layers.dense(x, units=1, activation=tf.nn.sigmoid)
         return x
+
+
+def generator(z, keep_prob=keep_prob, is_training=is_training):
+    activation = lrelu
+    momentum = 0.99
+    with tf.variable_scope("generator", reuse=None):
+        x = z
+        d1 = 4
+        d2 = 1
+        x = tf.layers.dense(x, units=d1 * d1 * d2, activation=activation)
+        x = tf.layers.dropout(x, keep_prob)      
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)  
+        x = tf.reshape(x, shape=[-1, d1, d1, d2])
+        x = tf.image.resize_images(x, size=[7, 7])
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=2, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=2, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=1, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=1, strides=1, padding='same', activation=tf.nn.sigmoid)
+        return x
